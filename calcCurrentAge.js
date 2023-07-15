cc01f33e1f22e623e75828abc607d21da778900f
Luiz Franzon <luizfranzon05@hotmail.com>
2023-07-14 22:28:05 -0300

    added age calculator

---
+++ calcCurrentAge.js
@@ -0,0 +1,14 @@
+const Mustache = require('mustache');
+const fs = require('fs');
+const mustachePath = './main.mustache';
+const birthDate = new Date(2003, 7, 5); // 5th August 2003
+const currentDate = new Date();
+const millisecondsInAYear = 31536000000;
+
+const generateReadme = () => {
+  const actualAge = Math.floor((currentDate - birthDate) / millisecondsInAYear);
+  const output = Mustache.render(fs.readFileSync(mustachePath, 'utf8'), { age: actualAge });
+  fs.writeFileSync('README.md', output);
+};
+
+generateReadme();
