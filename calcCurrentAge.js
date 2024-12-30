f4eedc05adde93b91309fd61e89696d6b3fd699d
Luiz <luizfranzon05@hotmail.com>
2024-12-30 17:45:21 -0300

    aaa

--- calcCurrentAge.js
+++ calcCurrentAge.js
@@ -1,13 +1,25 @@
-const fs = require('fs')
-const Mustache = require('mustache')
-const { differenceInYears, format } = require("date-fns")
-const { ptBR } = require("date-fns/locale/pt-BR")
+const { differenceInYears, format } = require("date-fns");
+const { ptBR } = require("date-fns/locale/pt-BR");
+const Mustache = require("mustache");
+const fs = require("fs");
 
-const generateReadme = () => {
-  const actualAge = differenceInYears(new Date(), new Date(2003, 7, 5))
-  const generatedAtDate = format(new Date(), "dd 'de' MMMM 'de' yyyy", { locale: ptBR })
-  const output = Mustache.render(fs.readFileSync('./main.mustache', 'utf8'), { age: actualAge, generatedAt: generatedAtDate })
-  fs.writeFileSync('README.md', output)
-};
+async function generateReadme() {
+  const actualAge = differenceInYears(new Date(), new Date(2003, 7, 5));
+
+  const generatedAtDate = format(
+    new Date(),
+    "dd 'de' MMMM 'de' yyyy 'às' HH:mm:ss",
+    {
+      locale: ptBR,
+    }
+  );
+
+  const output = Mustache.render(fs.readFileSync("./main.mustache", "utf8"), {
+    age: actualAge,
+    generatedAt: generatedAtDate,
+  });
+
+  fs.writeFileSync("README.md", output);
+}
 
 generateReadme();
